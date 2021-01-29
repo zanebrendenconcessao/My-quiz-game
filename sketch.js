017c1911478f07dd73b046217e03fb912a4257bc
zane brenden <zanebrenden2@gmail.com>
2021-01-29 13:50:51 +0530

    initial commit

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var gamestate=0;
+var contestantcount;
+var database;
+var quiz;
+var question;
+var contestant;
+
+
+function preload(){
+  
+}
+
+function setup(){
+  canvas = createCanvas(850,400);
+ 
+}
+
+
+function draw(){
+  
+  
+}
